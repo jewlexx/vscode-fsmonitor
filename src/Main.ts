f5793baf1d15dc9fb6bc431b7fc260e6c664def8
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-07-28 09:50:23 +1000

    Updated and fixed some things

--- src/Main.ts
+++ src/Main.ts
@@ -8,6 +8,8 @@ export default class Extension {
 	oldFileSize = '0 B';
 	oldDirSize = '0 B';
 	enabled: boolean;
+	idName = 'fsMonitor';
+	displayName = 'FS Monitor';
 
 	constructor(public context: vscode.ExtensionContext) {
 		// Bindings so anything done in the following functions is done in the context of the extension
@@ -19,16 +21,16 @@ export default class Extension {
 		this.toggleOnOff = this.toggleOnOff.bind(this);
 
 		// Sets the configuration to the users configuration
-		this.configuration = vscode.workspace.getConfiguration('fsMonitor');
+		this.configuration = vscode.workspace.getConfiguration(this.idName);
 
 		this.enabled =
-			this.configuration.get<boolean>('fsMonitor.enabled') || false;
+			this.configuration.get<boolean>(`${this.idName}.enabled`) || false;
 
 		// Creates the status bar item and uses the values from config
 		this.createStatusBarItem();
 
 		const toggleOnOffCommand = vscode.commands.registerCommand(
-			'fsMonitor.toggleOnOff',
+			`${this.idName}.toggleOnOff`,
 			this.toggleOnOff
 		);
 
@@ -51,7 +53,7 @@ export default class Extension {
 		// Initializes the status bar
 		this.updateStatusBar();
 
-		console.log('FS Monitor was successfully activated!');
+		console.log(`${this.displayName} was successfully activated!`);
 	}
 
 	createStatusBarItem() {
@@ -61,49 +63,49 @@ export default class Extension {
 		const alignment = alignmentConfig === 'right' ? 'Right' : 'Left';
 
 		const item = vscode.window.createStatusBarItem(
-			'fsMonitor.fileSizeStatus',
+			`${this.idName}.fileSizeStatus`,
 			vscode.StatusBarAlignment[alignment],
 			this.configuration.get<number>('priority')
 		);
 
-		item.command = 'fsMonitor.toggleOnOff';
-		item.tooltip = 'Toggle FS Monitor On/Off';
+		item.command = `${this.idName}.toggleOnOff`;
+		item.tooltip = `Toggle ${this.displayName} On/Off`;
 
 		if (!this.enabled) {
 			item.text = 'Disabled';
 			item.backgroundColor = new vscode.ThemeColor(
 				'statusBarItem.errorBackground'
 			);
-			console.log('FS Monitor has been disabled!');
+			console.log(`${this.displayName} has been disabled!`);
 		}
 
 		return item;
 	}
 
 	async toggleOnOff() {
-		this.configuration.update('fsMonitor.enabled', !this.enabled);
+		this.configuration.update(`${this.idName}.enabled`, !this.enabled);
 		if (this.enabled) {
 			this.enabled = false;
 			this.fileSizeItem.text = 'Disabled';
 			this.fileSizeItem.backgroundColor = new vscode.ThemeColor(
 				'statusBarItem.errorBackground'
 			);
-			console.log('FS Monitor has been disabled!');
+			console.log(`${this.displayName} has been disabled!`);
 		} else {
 			this.enabled = true;
 			this.fileSizeItem.backgroundColor = undefined;
 			this.fileSizeItem.text = 'Enabling...';
 			this.updateStatusBar();
-			console.log('FS Monitor has been enabled!');
+			console.log(`${this.displayName} has been enabled!`);
 		}
 	}
 
 	async updateConfiguration(e: vscode.ConfigurationChangeEvent) {
-		if (!e.affectsConfiguration('fsMonitor')) {
+		if (!e.affectsConfiguration(this.idName)) {
 			return;
 		}
 
-		this.configuration = vscode.workspace.getConfiguration('fsMonitor');
+		this.configuration = vscode.workspace.getConfiguration(this.idName);
 
 		this.fileSizeItem.hide();
 		this.fileSizeItem.dispose();
@@ -114,7 +116,9 @@ export default class Extension {
 	}
 
 	async updateStatusBar() {
-		if (!this.enabled) return this.fileSizeItem.show();
+		if (!this.enabled) {
+			return this.fileSizeItem.show();
+		}
 
 		const currentFileSize = await this.getFileSize();
 
@@ -187,7 +191,7 @@ export default class Extension {
 
 	async getFolderSize(uri: vscode.Uri) {
 		const ignoreNodeModules = vscode.workspace
-			.getConfiguration('fsMonitor')
+			.getConfiguration(this.idName)
 			.get<boolean>('ignoreNodeModules');
 
 		const ffsConfig: ffs.Options | undefined = ignoreNodeModules
